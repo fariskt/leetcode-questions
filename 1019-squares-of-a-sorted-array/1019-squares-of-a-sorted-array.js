2b81cb0a417f6a884c5bbe670ee20c432cf4c10a
fariskt <113295885+fariskt@users.noreply.github.com>
2024-11-12 09:54:54 +0530

    Time: 10 ms (38.89%), Space: 56.7 MB (48.8%) - LeetHub

--- 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.js
+++ 1019-squares-of-a-sorted-array/1019-squares-of-a-sorted-array.js
@@ -5,7 +5,7 @@
 var sortedSquares = function(nums) {
   let arr = [];
   for(let i=0;i<nums.length;i++){
-     let newRR= nums[i] * nums[i]     
+     let newRR = nums[i] * nums[i]     
      arr.push(newRR)
   }
   return arr.sort((a,b)=> a-b);
